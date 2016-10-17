be5a7fce5dcf7ad4332609e0945629096b6a9e05
Joao Felipe de Medeiros Moreira <jfmedeiros1820@gmail.com>
2016-10-17 21:53:23 -0200

    ConnectionFactory created

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,62 @@
+var ConnectionFactory = (function() {
+    
+    const stores = ['negociacoes'];
+    const version = 1;
+    const dbName = 'aluraframe';
+
+    var connection;
+
+    var close;
+
+    class ConnectionFactory {
+
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                }
+
+                openRequest.onsuccess = e => {
+
+                    if(!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function(){
+                            throw new Error('A conexão não pode ser fechada diretamente.');
+                        }
+                    }
+                    resolve(connection);
+                }
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+
+                    reject(e.target.error.name);
+                }
+            });
+        }
+
+        static _createStores(connection){
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)){
+                    connection.deleteObjectStore(store)
+                }
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
+
+        static closeConnection(){
+            if(connection){
+                close();
+                connection = null;
+            }
+        }
+    }
+})();
